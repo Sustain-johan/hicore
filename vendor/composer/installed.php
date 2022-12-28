482eb5178158e8bb7cccbe1c4cfa2eaad75b65fa
Sustain-johan <112610776+Sustain-johan@users.noreply.github.com>
2022-12-28 10:55:35 +0100

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '00f930694731ee816048da85d8c133ca6b412092',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '00f930694731ee816048da85d8c133ca6b412092',
+    ),
+    'automattic/woocommerce' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a71aa95cc3de3f1d68c6303525d03c0557a96137',
+    ),
+  ),
+);
